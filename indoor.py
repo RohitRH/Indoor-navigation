961f07f6f56b930bbbaebc2108cbb7905fd97019
Suhas B Kowligi <kowligi1998@gmail.com>
2019-06-11 12:13:15 +0530

    database creation

--- indoor.py
+++ indoor.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/sample"
+mongo = PyMongo(app)
 
 @app.route('/',methods=['GET','POST'])
 def home():
-    return '<strong style="font-size:50px;">indoor bitch</strong>'
+    return(str((mongo.db.name)))
 
 if __name__ == '__main__':
     app.run(debug=True)
