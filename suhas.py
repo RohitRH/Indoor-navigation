a16a05cf4f9522b6d065f49b86a2330bd35f2334
Suhas B Kowligi <kowligi1998@gmail.com>
2019-06-17 12:56:55 +0530

    updates

--- suhas.py
+++ suhas.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 from flask import Flask,request
-from flask_pymongo import PyMongo
+import pymongo
 from flask_restful import Api,Resource
 import json
 from json import *
 from bson.json_util import dumps
-import base64
+import base64,random
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/sample"
-mongo = PyMongo(app)
+app.config["MONGO_URI"] = ""
+client=pymongo.MongoClient("mongodb://suhas:suhas@owner-shard-00-00-gwlya.mongodb.net:27017,owner-shard-00-01-gwlya.mongodb.net:27017,owner-shard-00-02-gwlya.mongodb.net:27017/owner?ssl=true&replicaSet=owner-shard-0&authSource=admin&retryWrites=true&w=majority")
+db = client.owner
+
 api = Api(app)
 
 class Search(Resource):
@@ -18,8 +20,7 @@ class Search(Resource):
         try:
             item = data['item']
             category = data['category']
-
-            all_stores = mongo.db.sample.find()
+            all_stores = db.itm.find()
 
             ratings = []
             prices = []
@@ -30,7 +31,7 @@ class Search(Resource):
                 if category in store['comm'].keys() and item in store['comm'][category].keys():
                     stores.append(store['sname'])
                     ratings.append(store['comm'][category][item]['rating'])
-                    prices.append(store['comm'][category][item]['price'])
+                    prices.append(int(store['comm'][category][item]['price']))
                     quant.append(store['comm'][category][item]['quantity'])
 
             final_list = list(zip(stores,prices,ratings,quant))
@@ -52,23 +53,24 @@ class Search(Resource):
 class Map(Resource):
     def get(self):
         # store the images in the database as string:
-        f = open('img1.jpg','rb').read()
-        data = base64.b64encode(f).decode('ascii')
+        f = open('floorplan/'+random.choice(['img1.jpg','img2.jpg','img3.jpg']),'rb')
+        data = base64.b64encode(f.read()).decode('utf-8')
 
-        return {'response':data}
+        return {'name':f.name,'response':data}
 
     def post(self):
         data = request.get_json()
         try:
-            bid = data['bid']
-            src = data['src']
+            bid = data['src']
             dest = data['dest']
 
-            store = mongo.db.sample.indoor.find({'bid':bid,'src':src,'dest':dest})[0]
+            store = db.indoor.find({'src':bid,'dest':dest})[0]
 
-            img = store['img']
+            data = store['img']
 
-            return({'error':False,'response':img})
+            #data = base64.b64encode(img).decode('utf-8')
+
+            return({'error':False,'response':data})
 
         except KeyError as e:
             print("key error "+str(e))
@@ -78,8 +80,28 @@ class Map(Resource):
             print("No such route "+str(e))
             return {'error':True},400
 
+class Marketing(Resource):
+    def post(self):
+        data = request.get_json()
+        try:
+            bid = data['bid']
+
+            store = db.itm.find({'bid':bid})
+
+            for i in store:
+                ads = i['ads']
+                coupons = i['cpn_des']
+
+            data = {'ads':ads,'coupons':coupons}
+
+            return {'error':False,'response':data}
+        except KeyError:
+            return {'error':True}
+
+
 api.add_resource(Search,'/search')
 api.add_resource(Map,'/map')
+api.add_resource(Marketing,'/marketing')
 
 if __name__ == '__main__':
     app.run(debug=True)
